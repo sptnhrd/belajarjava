e2ad30b79fb4d597cd9d49793a3950e1b51575ea
sptnhrd <112158068+sptnhrd@users.noreply.github.com>
2022-10-11 07:48:51 +0700

    Update MUDAH3.java

--- MUDAH3.java
+++ MUDAH3.java
@@ -1,12 +1,3 @@
-/*TUGAS PERTEMUAN KE-01
-PEMROGRAMAN BERORIENTASI OBJEK, GANJIL 2022/2023
-NPM : 2113020013
-Nama : Septian Hariadi
-Kelas : 2J
-Kategori : Mudah
-Soal : Menentukan faktorial suatu bilangan yang diinputkan
-*/
-
 import java.util.*;
 
 public class MUDAH3 {
